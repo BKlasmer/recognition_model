99d813c4eeaa93b38f6f1e05b7502e540e58a2da
Ben Klasmer <benjamin_aram@hotmail.com>
2021-02-26 11:52:28 +0000

    Add paths to player data

--- recognition_model/dataloader.py
+++ recognition_model/dataloader.py
@@ -9,6 +9,14 @@ and creating image pairs and labels for the siamese network.
 """
 
 class DataLoader(object):
-    def __init__(self, logger_level: str = "INFO") -> None:
+    def __init__(self, root: str, logger_level: str = "INFO") -> None:
         self._logger = Logging().create_logger(logger_name="Data Loader", logger_level=logger_level)
         self._logger.info("Initialising Data Loader")
+        self._root = root
+
+        paths_per_person = []
+        for team_dir in filter(Path.is_dir, Path(root).iterdir()):
+            for player_dir in filter(Path.is_dir, team_dir.iterdir()):
+                paths_per_person.append(list(map(str, player_dir.glob("*.jpg"))))
+
+        self.paths_per_person = [paths for paths in paths_per_person if len(paths) > 1]

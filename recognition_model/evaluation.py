7a3cd12cb26c08fa960bb2f391c161b9619bf4f9
Ben Klasmer <benjamin_aram@hotmail.com>
2021-03-02 16:53:01 +0000

    Method to compute AUC

--- recognition_model/evaluation.py
+++ recognition_model/evaluation.py
@@ -2,13 +2,33 @@
 # -*- coding: latin-1 -*-
 
 import random
+import matplotlib.pyplot as plt
 from recognition_model.utils import Logging
+from torch.utils.data import DataLoader as TorchLoader
+from sklearn.metrics import roc_curve, auc, fbeta_score
+
 
 """Class for evaluating the model
 """
 
 class Evaluation(object):
-    def __init__(self, random_seed: int = 42, logger_level: str = "INFO") -> None:
+    def __init__(self, logger_level: str = "INFO") -> None:
         self._logger = Logging().create_logger(logger_name="Evaluation", logger_level=logger_level)
         self._logger.info("Initialising Evaluation")
-        self._logger.info(f"Setting random seed to: {random_seed}")
+
+    def calculate_auc(self, model, test_loader):
+
+        loader = TorchLoader(test_loader, batch_size=250)
+
+        for batch in loader:
+            img1, img2, labels = batch
+            break
+
+        predicted_scores = model(img1, img2)
+        predicted_scores = predicted_scores.detach().numpy()
+        labels = labels.numpy()
+
+        fpr, tpr, thresholds = roc_curve(labels, predicted_scores)
+        roc_auc = auc(fpr, tpr)
+
+        return roc_auc, fpr, tpr, thresholds
